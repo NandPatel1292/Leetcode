f218011721a21b5c809d6086f268e02117379d50
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-03-23 13:11:13 +0530

    Time: 144 ms (54.33%), Space: 68.1 MB (5.24%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void bfs(int n,vector<int>&visit,vector<vector<int>>&adj){
+        queue<int>q;
+        q.push(n);
+        visit[n] = 1;
+        
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            for(auto i: adj[x]){
+                if(!visit[i]){
+                    visit[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
+        
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& c) {
+        if(c.size() < n-1){
+            return -1;
+        }
+        vector<vector<int>> adj(n+1);
+        for(auto& i:c){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        
+        vector<int>visit(n+1,0);
+        int cnt = 0;
+        for(int i = 0; i<n; i++){
+            if(!visit[i]){
+                cnt++;
+                bfs(i,visit,adj);
+            }
+        }
+        
+        return cnt-1;
+    }
+};
