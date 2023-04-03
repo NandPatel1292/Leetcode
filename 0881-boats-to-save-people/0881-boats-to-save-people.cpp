4d5457dcafc79248535f711ea7f224a1b9ba0183
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-04-03 11:23:41 +0530

    Time: 90 ms (52.24%), Space: 41.9 MB (59.84%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int limit) {
+        sort(p.begin(),p.end());
+        
+        int ans = 0;
+        int right = p.size()-1;
+        int left = 0;
+        
+        while(left<=right){
+            if(p[left] + p[right] <= limit){
+                left++;
+                right--;
+            }
+            else{
+                right--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
