59acd9a36636e62140bc01d98818a8f91d9c3279
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-05-17 23:14:01 +0530

    Time: 277 ms (38.29%), Space: 126.7 MB (23.20%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,33 +11,53 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
+//         int ans = 0;
+        
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+        
+//         while(fast != NULL && fast->next != NULL){
+//             fast = fast->next->next;
+//             slow = slow->next;
+//         }
+        
+//         ListNode* nx = NULL;
+//         ListNode* prev = NULL;
+        
+//         while(slow != NULL){
+//             nx = slow->next;
+//             slow->next = prev;
+//             prev = slow;
+//             slow = nx;
+//         }
+        
+//         ListNode* t1 = head;
+//         ListNode* t2 = prev;
+        
+//         while(t2 != NULL){
+//             ans = max(ans,t2->val + t1->val);
+//             t1 = t1->next;
+//             t2 = t2->next;
+//         }
+        
+//         return ans;
+        
         int ans = 0;
+        vector<int> v;
+        ListNode* tmp = head;
         
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast != NULL && fast->next != NULL){
-            fast = fast->next->next;
-            slow = slow->next;
+        while(tmp != NULL){
+            v.push_back(tmp->val);
+            tmp = tmp->next;
         }
         
-        ListNode* nx = NULL;
-        ListNode* prev = NULL;
+        int n = 0;
+        int m = v.size()-1;
         
-        while(slow != NULL){
-            nx = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nx;
-        }
-        
-        ListNode* t1 = head;
-        ListNode* t2 = prev;
-        
-        while(t2 != NULL){
-            ans = max(ans,t2->val + t1->val);
-            t1 = t1->next;
-            t2 = t2->next;
+        while(n <= m){
+            ans = max(ans,v[n] + v[m]);
+            n++;
+            m--;
         }
         
         return ans;
