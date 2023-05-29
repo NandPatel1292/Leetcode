325a11635eafd93fc999a8cce1381688c1a6d777
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-05-29 09:39:19 +0530

    Time: 66 ms (43.21%), Space: 33 MB (60.01%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,19 +1,24 @@
 class ParkingSystem {
 public:
-    int cb, cm, cs;
-    int b,m,s;
+    // int cb, cm, cs;
+    // int b,m,s;
+    vector<int>limit;
+    vector<int>cnt;
+    
     ParkingSystem(int big, int medium, int small) {
-        this->b = big;
-        this->m = medium;
-        this->s = small;
+        // this->b = big;
+        // this->m = medium;
+        // this->s = small;
+        this->limit = {big,medium,small};
         
-        this->cb = 0;
-        this->cm = 0;
-        this->cs = 0;
+        // this->cb = 0;
+        // this->cm = 0;
+        // this->cs = 0;
+        this->cnt = {0,0,0};
     }
     
     bool addCar(int t) {
-        if(t == 1){
+/*        if(t == 1){
             if(this->cb < this->b){
                 this->cb++;
                 return true;
@@ -40,7 +45,13 @@ public:
                 return false;
             }
         }
+    */
+    if(this->cnt[t-1] < this->limit[t-1]){
+        this->cnt[t-1]++;
+        return true;
     }
+    return false;
+}
 };
 
 /**
