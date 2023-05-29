e6f79cd26061c1f00ba6be5211c586130b7f6d8b
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-05-29 11:02:48 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (55.79%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -7,28 +7,60 @@ public:
     }
     
     void push(int x) {
-        while(!q1.empty()){
+        // while(!q1.empty()){
+        //     tmp = q1.front();
+        //     q2.push(tmp);
+        //     q1.pop();
+        // }
+        // q1.push(x);
+        // while(!q2.empty()){
+        //     tmp = q2.front();
+        //     q1.push(tmp);
+        //     q2.pop();
+        // }
+        q1.push(x);
+    }
+    
+    int pop() {
+        // tmp = q1.front();
+        // q1.pop();
+        // return tmp;
+        while(q1.size()>1){
             tmp = q1.front();
             q2.push(tmp);
             q1.pop();
         }
-        q1.push(x);
-        while(!q2.empty()){
-            tmp = q2.front();
-            q1.push(tmp);
-            q2.pop();
-        }
-    }
-    
-    int pop() {
-        tmp = q1.front();
+        
+        int x = q1.front();
         q1.pop();
-        return tmp;
+        // while(!q2.empty()){
+        //     int tmp = q2.front();
+        //     q1.push(tmp);
+        //     q2.pop();
+        // }
+        swap(q1,q2);
+        return x;
     }
     
     int top() {
-        tmp = q1.front();
-        return tmp;
+        // tmp = q1.front();
+        // return tmp;
+        while(q1.size() > 1){
+            tmp = q1.front();
+            q2.push(tmp);
+            q1.pop();
+        }
+        int x = q1.front();
+        q2.push(q1.front());
+        q1.pop();
+        // while(!q2.empty()){
+        //     int tmp = q2.front();
+        //     q1.push(tmp);
+        //     q2.pop();
+        // }
+        swap(q1,q2);
+        return x;
+        
     }
     
     bool empty() {
