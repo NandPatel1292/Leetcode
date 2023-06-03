0ff65487139fa1c2b83addf444608aea253bbd4d
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-03 20:24:05 +0530

    Time: 339 ms (44.91%), Space: 112.7 MB (86.03%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,8 +1,17 @@
 class Solution {
+
 public:
+        
+    int ans = INT_MIN;
+    void dfs(vector<int> adj[],int headID, vector<int>&time,int curr){
+        ans = max(ans,curr);
+        
+        for(auto i : adj[headID]){
+            dfs(adj,i,time,curr+time[headID]);
+        }
+    }
     int numOfMinutes(int n, int headID, vector<int>& m, vector<int>& time) {
         vector<int> adj[n];
-        int ans = INT_MIN;
         
         for(int i = 0; i<n; i++){
             if(m[i] != -1){
@@ -10,21 +19,24 @@ public:
             }
         }
         
-        queue<pair<int,int>> q;
-        q.push({headID,0});
+//         queue<pair<int,int>> q;
+//         q.push({headID,0});
         
-        while(!q.empty()){
-            auto x = q.front();
-            q.pop();
-            int head = x.first;
-            int t = x.second;
+//         while(!q.empty()){
+//             auto x = q.front();
+//             q.pop();
+//             int head = x.first;
+//             int t = x.second;
             
-            ans = max(ans,t);
+//             ans = max(ans,t);
             
-            for(auto &i : adj[head]){
-                q.push({i,t+time[head]});
-            }
-        }
+//             for(auto &i : adj[head]){
+//                 q.push({i,t+time[head]});
+//             }
+//         }
+        
+        
+        dfs(adj,headID,time,0);
         
         return ans;
     }
