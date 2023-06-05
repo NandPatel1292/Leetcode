9a0cec5a74ced6e2718c7caed3afae10ba233bee
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-05 18:32:24 +0530

    Time: 10 ms (61.35%), Space: 10 MB (63.27%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+        int n = c.size();
+        
+        if(n <= 2){
+            return true;
+        }
+          
+        for(int i = 2; i<n; i++){
+            if((c[i][0] - c[0][0])*(c[1][1] - c[0][1]) != (c[i][1] - c[0][1])*(c[1][0] - c[0][0])){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
