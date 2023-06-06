3107cda6510298a09d32637cab74ccc435979707
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-06 10:15:13 +0530

    Time: 5 ms (35.85%), Space: 9.3 MB (5.60%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
+        // sort(arr.begin(),arr.end());
+        int mi = *min_element(arr.begin(),arr.end());
+        int mx = *max_element(arr.begin(),arr.end());
+        int n = arr.size();
         
-        int dif = arr[1] - arr[0];
-        for(int i = 0; i<arr.size()-1; i++){
-            if(arr[i+1] - arr[i] != dif){
-                return false;
-            }
+        if(mx - mi == 0){
+            return true;
         }
         
-        return true;
+        if((mx - mi)%(n-1) != 0){
+            return false;
+        }
+        
+        int dif = (mx - mi)/(n-1);
+        unordered_set<int> s;
+        // int dif = arr[1] - arr[0];
+        for(int i = 0; i<n; i++){
+            // if(arr[i+1] - arr[i] != dif){
+            //     return false;
+            // }
+            
+            if((arr[i] - mi)% dif != 0){
+                return false;
+            }
+            s.insert(arr[i]);
+        }
+        
+        return s.size() == n;
         
     }
 };
