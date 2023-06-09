1dd3bea7eaa954d1d51a8c9c03ca3932d4d03918
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-09 09:22:27 +0530

    Time: 26 ms (5.25%), Space: 15.9 MB (72.43%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& l, char target) {
-        if(target >= l[l.size()- 1]){
-            return l[0];
+        // if(target >= l[l.size()- 1]){
+        //     return l[0];
+        // }
+        // else{
+        //     int index = upper_bound(l.begin(),l.end(),target) - l.begin();
+        //     // cout<<index;
+        //     return l[index];    
+        // }
+        
+        int left = 0;
+        int right = l.size() - 1;
+        int mid;
+        while(left <= right){
+            mid  =  left + (right - left)/2;
+            if(l[mid] <= target){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
         }
-        else{
-            int index = upper_bound(l.begin(),l.end(),target) - l.begin();
-            // cout<<index;
-            return l[index];    
-        }
+        
+        return left == l.size() ? l[0] : l[right+1];
         
     }
 };
