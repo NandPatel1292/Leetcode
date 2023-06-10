f06bf5936f76a5d704ecc80bcf55f38185bb9bf6
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-10 22:25:02 +0530

    Time: 3 ms (95.18%), Space: 11.4 MB (50.39%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
-    void solve(string s, set<string>&ans, string op){
+    void solve(string s, vector<string>&ans, string op){
         if(s.size() == 0){
-            ans.insert(op);
+            ans.push_back(op);
             return;
         }
         
-        string op1 = op;
-        string op2 = op;
+       
         
-        op1.push_back(tolower(s[0]));
-        op2.push_back(toupper(s[0]));
+        if(isalpha(s[0])){
+            string op1 = op;
+            string op2 = op;
+            
+            op1.push_back(tolower(s[0]));
+            op2.push_back(toupper(s[0]));
+            
+            s.erase(s.begin());
+            
+            solve(s,ans,op1);
+            solve(s,ans,op2);
+        }
+        else{
+            string op1 = op;
+            op1.push_back(s[0]);
+            s.erase(s.begin());
+            solve(s,ans,op1);
+        }
         
-        s.erase(s.begin());
         
-        solve(s,ans,op1);
-        solve(s,ans,op2);
+        
+        
+        
+        
     }
     
     
     vector<string> letterCasePermutation(string s) {
         vector<string> tmp;
-        set<string>st;
         string op = "";
-        solve(s,st,op);
         
-        for(auto x : st){
-            tmp.push_back(x);
-        }
+        solve(s,tmp,op);
+        
         return tmp;
     }
 };
