9cbead4641a71e0ed9c22d9208afa583267e3561
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-13 11:18:58 +0530

    Time: 313 ms (24.15%), Space: 42.6 MB (24.99%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -4,19 +4,40 @@ public:
         int c = 0;
         int n = g.size();
         
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                int f = 1;
-                for(int k = 0; k<n; k++){
-                    if(g[i][k] != g[k][j]){
-                        f = 0;
-                        break;
-                    }
-                }
-                if(f){
-                    c++;
-                }
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<n; j++){
+//                 int f = 1;
+//                 for(int k = 0; k<n; k++){
+//                     if(g[i][k] != g[k][j]){
+//                         f = 0;
+//                         break;
+//                     }
+//                 }
+//                 if(f){
+//                     c++;
+//                 }
               
+//             }
+//         }
+        
+        
+        unordered_map<string,int> mp;
+        for(int i = 0; i<n; i++){
+            string ans = "";
+            
+            for(int j = 0; j<n; j++){
+                ans += to_string(g[i][j]) + "N";
+            }
+            mp[ans]++;
+        }
+        
+        for(int i = 0; i<n; i++){
+                string chk = "";
+            for(int j = 0; j<n; j++){
+                chk += to_string(g[j][i]) + "N";
+            }
+            if(mp.find(chk) != mp.end()){
+                c = c + mp[chk];
             }
         }
         
