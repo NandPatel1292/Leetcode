259d00cef15b2a11b5c3f46ea508a83fcf09ab96
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-22 23:13:05 +0530

    Time: 2 ms (93.41%), Space: 7 MB (90.67%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -63,14 +63,39 @@ public:
         }
     }
     
+    void solve2(int col, vector<vector<string>>&ans,vector<string>&p,int n,vector<int>&left,vector<int>&uppr,vector<int>&lower){
+        if(col == n){
+            ans.push_back(p);
+            return;
+        }
+        
+        for(int i = 0; i<n; i++){
+            if(left[i] == 0 && lower[i+col] == 0 && uppr[n-1+col-i] == 0){
+                p[i][col] = 'Q';
+                left[i] = 1;
+                uppr[n-1 + col - i] = 1;
+                lower[i+col] = 1;
+                solve2(col+1,ans,p,n,left,uppr,lower);
+                p[i][col] = '.';
+                left[i] = 0;
+                uppr[n-1+col-i] = 0;
+                lower[i+col] = 0;
+            }
+        }
+    }
+    
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         string s(n,'.');
         vector<string>p(n,s);
         
         
-        solve(0,ans,p,n);
+        // solve(0,ans,p,n);
         
+        vector<int>left(n,0);
+        vector<int> uppr(2*n-1,0);
+        vector<int>lower(2*n-1,0);
+        solve2(0,ans,p,n,left,uppr,lower);
         return ans;
     }
 };
