3d7da16792ecd750bfbf5a1f409d37d90972532f
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-22 11:35:26 +0530

    Time: 8 ms (15.85%), Space: 7.9 MB (40.80%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -16,10 +16,24 @@ public:
         }
     }
     
+    
+    void solve2(int ind, vector<int>n, vector<vector<int>>&ans){
+        if(ind == n.size()){
+            ans.push_back(n);
+            return;
+        }
+        
+        for(int i = ind; i<n.size(); i++){
+            swap(n[ind],n[i]);
+            solve2(ind+1,n,ans);
+            swap(n[ind],n[i]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& n) {
         vector<vector<int>>ans;
         vector<int>op;
-        solve(0,op,n,ans);
+        // solve(0,op,n,ans);
+        solve2(0,n,ans);
         
         return ans;
     }
