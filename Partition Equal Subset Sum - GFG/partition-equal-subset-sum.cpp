8a05beab7682d5af71cd5e556b94781339384a4f
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-06-29 16:38:49 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -45,9 +45,28 @@ public:
         
         int tar = sum/2;
         
-        vector<vector<int>> dp(n,vector<int>(tar+1,-1));
+        // vector<vector<int>> dp(n,vector<int>(tar+1,-1));
+        // return solve(0,arr,n,tar,dp);
         
-        return solve(0,arr,n,tar,dp);
+        vector<vector<int>>dp(n+1,vector<int>(tar+1));
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = true;
+        }
+        for(int j = 1; j<= tar; j++){
+            dp[0][j] = false;
+        }
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=tar; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][tar];
     }
 };
 
