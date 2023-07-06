e8994727898a496168b6281efaf304a9f528eee5
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-07-06 23:02:19 +0530

    Time: 131 ms (23.04%), Space: 28.5 MB (44.20%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -19,10 +19,30 @@ public:
         return dp[m][n];
     }
     int minInsertions(string s) {
-        int n = s.size();
-        string ss = s;
-        reverse(ss.begin(),ss.end());
+//         int n = s.size();
+//         string ss = s;
+//         reverse(ss.begin(),ss.end());
         
-        return n - solve(s,ss,n,n);
+//         return n - solve(s,ss,n,n);
+        
+        string a = s;
+        reverse(s.begin(),s.end());
+        string b = s;
+        int n = s.size();
+        
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if(a[i-1] == b[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return n - dp[n][n];
     }
 };
