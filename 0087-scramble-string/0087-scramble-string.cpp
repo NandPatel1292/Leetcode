b67d0c6a586c89ad097bf97496f6dbcab4d26787
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-07-18 12:33:48 +0530

    Time: 538 ms (5.02%), Space: 84.6 MB (5.46%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,25 +1,66 @@
 class Solution {
 public:
-    bool isScramble(string s1, string s2) {
+    bool solve(string s1, string s2,unordered_map<string,bool>&mp){
+        if(s1.compare(s2) == 0){
+            return true;
+        }
+        if(s1.size()<=1){
+            return false;
+        }
+        
+        bool flg = false;
         int n = s1.size();
-        vector dp(n+1,vector(n,vector<int>(n)));
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                dp[1][i][j] = (s1[i] == s2[j]);
+        string key = s1 + ' ' + s2;
+        if(mp.find(key) != mp.end()){
+            return mp[key];
+        }
+        
+        for(int i = 1; i<n; i++){
+            bool c1 = false;
+            bool c2 = false;
+            if(solve(s1.substr(0,i),s2.substr(n-i,i),mp) && solve(s1.substr(i,n-i),s2.substr(0,n-i),mp)){
+                c1 = true;
+            }
+            if(solve(s1.substr(0,i),s2.substr(0,i),mp) && solve(s1.substr(i,n-i),s2.substr(i,n-i),mp)){
+                c2 = true;
+            }
+            
+            if(c1 || c2){
+                flg = true;
             }
         }
-        for(int k = 2; k<= n; k++){
-            for(int i = 0; i<n+1 - k; i++){
-                for(int j = 0; j< n+1 - k; j++){
-                    for(int nl = 1; nl < k; nl++){
-                        const vector<int>& dp1 = dp[nl][i];
-                        const vector<int>& dp2 = dp[k - nl][i+nl];
-                        dp[k][i][j] |= dp1[j] && dp2[j+nl];
-                        dp[k][i][j] |= dp1[j + k - nl] && dp2[j];
-                    }
-                }
-            }
+        
+        return mp[key] = flg;
+    }
+    
+    
+    bool isScramble(string s1, string s2) {
+        // int n = s1.size();
+        // vector dp(n+1,vector(n,vector<int>(n)));
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<n; j++){
+        //         dp[1][i][j] = (s1[i] == s2[j]);
+        //     }
+        // }
+        // for(int k = 2; k<= n; k++){
+        //     for(int i = 0; i<n+1 - k; i++){
+        //         for(int j = 0; j< n+1 - k; j++){
+        //             for(int nl = 1; nl < k; nl++){
+        //                 const vector<int>& dp1 = dp[nl][i];
+        //                 const vector<int>& dp2 = dp[k - nl][i+nl];
+        //                 dp[k][i][j] |= dp1[j] && dp2[j+nl];
+        //                 dp[k][i][j] |= dp1[j + k - nl] && dp2[j];
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp[n][0][0];
+        
+        unordered_map<string,bool>mp;
+        if(s1.size()!= s2.size()){
+            return false;
         }
-        return dp[n][0][0];
+        
+        return solve(s1,s2,mp);
     }
 };
