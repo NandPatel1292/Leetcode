06710edb7cae58a0852f4e9c7005b029409b04e2
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-08-08 15:25:49 +0530

    Time: 0 ms (100.00%), Space: 13.2 MB (40.02%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int sum = 0;
+        for(int i = 1; i<p.size(); i++){
+            sum += max(p[i] - p[i-1],0);
+        }
+        
+        return sum;
+    }
+};
