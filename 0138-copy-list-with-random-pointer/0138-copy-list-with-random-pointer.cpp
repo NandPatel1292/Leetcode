815381fdd3b84cbb2dce97481a07f484a80a2bfb
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-08-12 14:29:58 +0530

    Time: 3 ms (97.60%), Space: 11.3 MB (30.67%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,32 +19,48 @@ public:
     Node* copyRandomList(Node* head) {
         Node* n = head;
     
+//         while(n){
+//             Node* tmp = n->next;
+//             n->next = new Node(n->val);
+//             n->next->next = tmp;
+//             n = tmp;
+//         }
+        
+//         n = head;
+//         while(n){
+//             if(n->random){
+//                 n->next->random = n->random->next;
+//             }
+//             n = n->next->next;
+//         }
+        
+//         Node* ans = new Node(0);
+//         Node* res = ans;
+        
+//         while(head){
+//             res->next = head->next;
+//             res = res->next;
+            
+//             head->next = head->next->next;
+//             head = head->next;
+//         }
+        
+//         return ans->next;
+        
+        
+        unordered_map<Node*, Node*>mp;
         while(n){
-            Node* tmp = n->next;
-            n->next = new Node(n->val);
-            n->next->next = tmp;
-            n = tmp;
+            mp[n] = new Node(n->val);
+            n = n->next;
         }
         
         n = head;
         while(n){
-            if(n->random){
-                n->next->random = n->random->next;
-            }
-            n = n->next->next;
+            mp[n]->next = mp[n->next];
+            mp[n]->random = mp[n->random];
+            n = n->next;
         }
         
-        Node* ans = new Node(0);
-        Node* res = ans;
-        
-        while(head){
-            res->next = head->next;
-            res = res->next;
-            
-            head->next = head->next->next;
-            head = head->next;
-        }
-        
-        return ans->next;
+        return mp[head];
     }
 };
