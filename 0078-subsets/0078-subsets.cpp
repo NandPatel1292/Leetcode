210b57e971bce8bf230a95aabec17f6f96484a37
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-09-05 13:28:51 +0530

    Time: 4 ms (41.47%), Space: 16.8 MB (12.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -26,12 +26,29 @@ public:
         
     }
     
+    void solve2(int ind,vector<int>tmp,vector<int>n,vector<vector<int>>&ans){
+        if(ind == n.size()){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        int x = n[ind];
+        solve2(ind+1,tmp,n,ans);
+        tmp.push_back(x);
+        solve2(ind+1,tmp,n,ans);
+    }
+    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        // int sz = nums.size();
-        vector<int> tmp;
-        // ans.push_back(tmp);
-        solve(nums,ans,tmp);
+        // vector<vector<int>> ans;
+        // // int sz = nums.size();
+        // vector<int> tmp;
+        // // ans.push_back(tmp);
+        // solve(nums,ans,tmp);
+        
+        vector<vector<int>>ans;
+        vector<int>tmp;
+        
+        solve2(0,tmp,nums,ans);
         
         return ans;
     }
