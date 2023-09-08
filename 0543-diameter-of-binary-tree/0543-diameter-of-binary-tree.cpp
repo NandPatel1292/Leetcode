d20431afba46a53373a99adfa1852e9342c6a199
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-09-08 16:51:09 +0530

    Time: 10 ms (51.33%), Space: 20.2 MB (84.68%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -21,15 +21,16 @@ public:
         int r = solve(root->right,res);
         
         int tmp = max(l,r) +1;
-        int ans = max(tmp,l+r+1);
-        res = max(res,ans);
-        
+        // int ans = max(tmp,l+r+1);
+        // res = max(res,ans);
+        res = max(res,l+r);
         return tmp;
     }
     
     int diameterOfBinaryTree(TreeNode* root) {
         int res = INT_MIN;
         solve(root,res);
-        return res-1;
+        // return res-1;
+        return res;
     }
 };
