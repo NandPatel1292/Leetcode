637bc5e993f1e25d7685b7e1d01f1be88be45c76
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-10-25 18:09:29 +0530

    Time: 76 ms (83.53%), Space: 61.7 MB (37.36%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -14,13 +14,29 @@ public:
 //         }
 //         return ans;
         
-        sort(n.begin(),n.end());
-        for(int i = 0; i<n.size()-1; i++){
-            if(n[i] == n[i+1]){
-                ans = n[i];
-                break;
-            }
+        // sort(n.begin(),n.end());
+        // for(int i = 0; i<n.size()-1; i++){
+        //     if(n[i] == n[i+1]){
+        //         ans = n[i];
+        //         break;
+        //     }
+        // }
+        // return ans;
+        
+        int slow = n[0];
+        int fast = n[0];
+         
+        do{
+            fast = n[n[fast]];
+            slow = n[slow];
+        }while(fast != slow);
+        
+        fast = n[0];
+        while(fast != slow){
+            slow = n[slow];
+            fast = n[fast];
         }
-        return ans;
+        
+        return slow;
     }
 };
