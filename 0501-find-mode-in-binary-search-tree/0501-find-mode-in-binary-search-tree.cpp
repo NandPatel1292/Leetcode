8e84c0f22bcbdf7744df339f2c1b4d767e411f04
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2023-11-01 15:38:19 +0530

    Time: 11 ms (90.70%), Space: 25.8 MB (35.69%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -29,33 +29,64 @@ public:
     
     
     vector<int> findMode(TreeNode* root) {
-        vector<int>ans;
-        ans.push_back(root->val);
-        solve(root,ans);
+        // vector<int>ans;
+        // ans.push_back(root->val);
+        // solve(root,ans);
+        // unordered_map<int,int>mp;
+        // if(ans.size() == 1){
+        //     return ans;
+        // }
+        // else{
+        //     for(auto i : ans){
+        //         mp[i]++;
+        //     }
+        //     if(mp.size() == ans.size()){
+        //         return ans;
+        //     }
+        //     else{
+        //         int mx = 0;
+        //         vector<int>tmp;
+        //         for(auto x: mp){
+        //             mx = max(mx,x.second);
+        //         }
+        //         for(auto x : mp){
+        //             if(x.second == mx){
+        //                 tmp.push_back(x.first);
+        //             }
+        //         }
+        //         return tmp;
+        //     }
+        // }
+        
         unordered_map<int,int>mp;
-        if(ans.size() == 1){
-            return ans;
-        }
-        else{
-            for(auto i : ans){
-                mp[i]++;
+        queue<TreeNode*>q;
+        q.push(root);
+        
+        while(!q.empty()){
+            auto x = q.front();
+            q.pop();
+            
+            mp[x->val]++;
+            
+            if(x->left != NULL){
+                q.push(x->left);
             }
-            if(mp.size() == ans.size()){
-                return ans;
-            }
-            else{
-                int mx = 0;
-                vector<int>tmp;
-                for(auto x: mp){
-                    mx = max(mx,x.second);
-                }
-                for(auto x : mp){
-                    if(x.second == mx){
-                        tmp.push_back(x.first);
-                    }
-                }
-                return tmp;
+            if(x->right != NULL){
+                q.push(x->right);
             }
         }
+        
+        int mx = 0;
+        for(auto i : mp){
+            mx = max(mx,i.second);
+        }
+        vector<int>ans;
+        for(auto i : mp){
+            if(i.second == mx){
+                ans.push_back(i.first);
+            }
+        }
+        
+        return ans;
     }
 };
