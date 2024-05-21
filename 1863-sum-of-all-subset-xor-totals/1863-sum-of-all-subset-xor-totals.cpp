3bbe8559674400180d49c1b1fcadffd32c8f8957
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2024-05-21 23:56:08 +0530

    Time: 415 ms (5.05%), Space: 397.9 MB (5.32%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -12,8 +12,34 @@ public:
         return s + sx;
     }
     
-    int subsetXORSum(vector<int>& nums) {
-        return solve(nums,0,0);
+    void solve2(int ind, vector<int>&n, vector<int>&tmp, vector<vector<int>>&ans){
+        if(ind == n.size()){
+            ans.push_back(tmp);
+            return;
+        }
         
+        int x = n[ind];
+        solve2(ind+1,n,tmp,ans);
+        tmp.push_back(x);
+        solve2(ind+1,n,tmp,ans);
+    }
+    
+    int subsetXORSum(vector<int>& nums) {
+        // return solve(nums,0,0);
+        
+        vector<vector<int>>ans;
+        vector<int>tmp;
+        
+        solve2(0, nums, tmp, ans);
+        int result = 0;
+        for(auto i : ans){
+            int sum = 0;
+            for(auto j : i){
+                sum ^= j;
+            }
+            result += sum;
+        }
+        
+        return result;
     }
 };
