d73ab29c34fff6de01b9f3fa844ed823f819611b
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2024-05-31 11:04:39 +0530

    Time: 7 ms (62.82%), Space: 12.7 MB (35.15%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -3,15 +3,36 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         vector<int>ans;
         
-        unordered_map<int,int>mp;
-        for(auto i : nums){
-            mp[i]++;
+//         unordered_map<int,int>mp;
+//         for(auto i : nums){
+//             mp[i]++;
+//         }
+        
+//         for(auto i : mp){
+//             if(ans.size() < 2 && i.second < 2){
+//                 ans.push_back(i.first);
+//             }
+//         }
+        
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        while(i < nums.size()-1){
+            if(nums[i] == nums[i+1]){
+                i+=2;
+            }
+            else{
+               
+                ans.push_back(nums[i]);
+                 i++;
+                if(ans.size() == 2){
+                    return ans;
+                }
+                
+            }
         }
         
-        for(auto i : mp){
-            if(ans.size() < 2 && i.second < 2){
-                ans.push_back(i.first);
-            }
+        if(ans.size() == 1){
+            ans.push_back(nums[nums.size()-1]);
         }
         
         return ans;
