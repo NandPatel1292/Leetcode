b90913b54cd212715c4efcc06b409265b0e0f26e
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2024-05-31 11:20:34 +0530

    Time: 0 ms (100.00%), Space: 12.2 MB (98.70%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int>ans;
+        // vector<int>ans;
         
 //         unordered_map<int,int>mp;
 //         for(auto i : nums){
@@ -14,27 +14,46 @@ public:
 //             }
 //         }
         
-        sort(nums.begin(), nums.end());
-        int i = 0;
-        while(i < nums.size()-1){
-            if(nums[i] == nums[i+1]){
-                i+=2;
-            }
-            else{
+//         sort(nums.begin(), nums.end());
+//         int i = 0;
+//         while(i < nums.size()-1){
+//             if(nums[i] == nums[i+1]){
+//                 i+=2;
+//             }
+//             else{
                
-                ans.push_back(nums[i]);
-                 i++;
-                if(ans.size() == 2){
-                    return ans;
-                }
+//                 ans.push_back(nums[i]);
+//                  i++;
+//                 if(ans.size() == 2){
+//                     return ans;
+//                 }
                 
+//             }
+//         }
+        
+//         if(ans.size() == 1){
+//             ans.push_back(nums[nums.size()-1]);
+//         }
+        
+        
+        int ans = 0;
+        int mask = 1;
+        int first = 0;
+        
+        for(auto &i : nums){
+            ans ^= i;
+        }
+        
+        while(not (ans & mask)){
+            mask <<= 1;
+        }
+        
+        
+        for(auto &i : nums){
+            if(i & mask){
+                first ^= i;
             }
         }
-        
-        if(ans.size() == 1){
-            ans.push_back(nums[nums.size()-1]);
-        }
-        
-        return ans;
+        return {first,ans^first};
     }
 };
