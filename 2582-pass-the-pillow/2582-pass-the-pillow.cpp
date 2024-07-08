b3e9aaa227f69823255e184fde45b983a3e567c6
Nand Patel <92430136+NandPatel1292@users.noreply.github.com>
2024-07-08 16:18:44 +0530

    Time: 2 ms (53.64%), Space: 7.1 MB (37.53%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if(n > time){
+            return time+1;
+        }
+        if((time/(n-1))%2 == 0){
+            return time%(n-1)+1;
+        }
+        else{
+            return n - time%(n-1);
+        }
+    }
+};
